4e848689a37f40d087172b5a12daab9fb18235e6
Stéphane Lemay <interordi@interordi.com>
2021-11-29 22:48:45 -0500

    Get the database config in Bungee mode

--- src/main/java/com/interordi/iomawaba/IOMawabaBungee.java
+++ src/main/java/com/interordi/iomawaba/IOMawabaBungee.java
@@ -2,6 +2,14 @@ package com.interordi.iomawaba;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 import com.interordi.iomawaba.commands.GKick;
 import com.interordi.iomawaba.interfaces.PlayerActions;
@@ -21,7 +29,39 @@ public class IOMawabaBungee extends Plugin {
 	
 	public void onEnable() {
 		instance = this;
-		
+
+		//Create default configuration if not present
+		if (!getDataFolder().exists())
+			getDataFolder().mkdir();
+
+		File file = new File(getDataFolder(), "config.yml");
+
+		if (!file.exists()) {
+			try (InputStream in = getResourceAsStream("config.yml")) {
+				Files.copy(in, file.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		//Get the configuration data
+		Configuration configuration;
+		try {
+			configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+		} catch (IOException e) {
+			getLogger().severe("Failed to read the configuration file, stopping.");
+			return;
+		}
+
+		String dbHost = configuration.getString("database.host", null);
+		int dbPort = configuration.getInt("database.port", 3306);
+		String dbUsername = configuration.getString("database.username");
+		String dbPassword = configuration.getString("database.password");
+		String dbBase = configuration.getString("database.base");
+
+		db = new Database(dbHost, dbPort, dbUsername, dbPassword, dbBase);
+
+
 		PlayerActions pa = new PlayerActionsBungee();
 
 		getProxy().getPluginManager().registerListener(this, new PlayersListener());
