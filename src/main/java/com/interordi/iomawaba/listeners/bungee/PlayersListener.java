363c0294e2acf9527c2c279fecc1e4c5ab03facd
Stéphane Lemay <interordi@interordi.com>
2021-12-28 11:00:16 -0500

    Prevent chat notifications on the login attempt

--- src/main/java/com/interordi/iomawaba/listeners/bungee/PlayersListener.java
+++ src/main/java/com/interordi/iomawaba/listeners/bungee/PlayersListener.java
@@ -4,25 +4,32 @@ import com.interordi.iomawaba.modules.Bans;
 import com.interordi.iomawaba.utilities.BanData;
 
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class PlayersListener implements Listener {
 
 	@EventHandler
-	public void onPostLogin(PostLoginEvent event) {
+	public void onLoginEvent(LoginEvent event) {
 		BanData ban = Bans.getInstance().isBanned(
-			event.getPlayer().getUniqueId(),
-			event.getPlayer().getAddress().getHostString()
+			event.getConnection().getUniqueId(),
+			event.getConnection().getAddress().getHostString()
 		);
 
 		if (ban != null) {
 			String message = Bans.formatMessageTarget(ban);
 
-			event.getPlayer().disconnect(new TextComponent(message));
+			event.setCancelled(true);
+			event.setCancelReason(new TextComponent(message));
 		}
 
 	}
-	
+
+
+	@EventHandler
+	public void onServerConnectEvent(ServerConnectEvent event) {
+		//TODO: Server-specific check
+	}
 }
